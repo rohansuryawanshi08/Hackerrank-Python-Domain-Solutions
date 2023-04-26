187381465fd33dce883c0caf9cdd4ee27b443ed0
Rohan Suryawanshi <126396108+rohansuryawanshi08@users.noreply.github.com>
2023-04-26 14:07:14 +0530

    Create Mutations.py

---
+++ Strings/Mutations.py
@@ -0,0 +1,6 @@
+Link : https://www.hackerrank.com/challenges/python-mutations/problem
+    
+ def mutate_string(string, position, character):
+    return string[:position]+character+string[position+1:]
+
+if __name__ == '__main__':
